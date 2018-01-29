d31d58c19004636626873fd4435cab1705170826
equj <30753904+equj@users.noreply.github.com>
2018-01-29 03:03:44 -0500

    Update electroplottwo.R

--- electroplottwo.R
+++ electroplottwo.R
@@ -7,11 +7,11 @@ library(lubridate)
 colNewnames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 #read the file into variable commData
 electdata <- read.table("household_power_consumption.txt", col.names = colNewnames, na.strings = "?", sep = ";", skip = 66635, nrows = 2900)
-test$datetime <- dmy(test$Date) + hms(test$Time)
+electdata$datetime <- dmy(electdata$Date) + hms(electdata$Time)
 #Plot 2: Single line plot
 #opens PNG device
 png(filename = "Plot2.png", width = 480, height = 480, units = "px")
-plot(test$datetime, test$Global_active_power, type = "l", lty = 1, xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(electdata$datetime, electdata$Global_active_power, type = "l", lty = 1, xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.off()
 
 
